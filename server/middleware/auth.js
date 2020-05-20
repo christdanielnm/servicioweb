05433b4bd7ef6cf2858fe9dcf996b0cada859f36
Christian Nunez <christian96daniel@gmail.com>
2020-05-20 01:13:27 -0500

    primer commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,28 @@
+let jwt = require('jsonwebtoken')
+
+let is_auth = function (req,res,next)  {
+    let {authorization} = req.headers
+    console.log(authorization);
+    
+    if (!authorization) return res.status(401).send({
+        ok : false, 
+        message : 'falta token'
+    })
+    token = authorization.split(' ')[1]
+    jwt.verify(token , '123445', (err , decoded) => {
+        if (err) return res.status(401).send({
+            ok : false , 
+            message : 'acceso denegado'
+        })
+        req.info = decoded
+        next()
+        
+    })
+    
+ 
+    
+}
+
+module.exports = {
+    is_auth
+}
